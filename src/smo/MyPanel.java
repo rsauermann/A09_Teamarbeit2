1dbbd9d4e2ae63b0ff689f1b876c71495ef3a710
aoezer <aoezer@student.tgm.ac.at>
2015-12-17 09:13:14 +0100

    Die Klasse MyPanel erweitert und die Buttons initialisiert

--- src/smo/MyPanel.java
+++ src/smo/MyPanel.java
@@ -1,6 +1,7 @@
 package smo;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.util.Date;
 
 import javax.swing.*;
@@ -16,17 +17,22 @@ import javax.swing.*;
 public class MyPanel extends JFrame {
 	private JPanel all;//gesamt Panel
 	private JPanel north;//panel für den neuen game Button
-	private JPanel buttons;//hier kommen die 25 Buttons rein
+	private JPanel buttonsP;//hier kommen die 25 Buttons rein
+	private MyModel model;
+	private MyController controller;
 	
-	private JButton []buttonArray;
+	private JButton [][]buttonArray;
 	
 	private JButton newGame;//Generiert das Spielfeld neu
 	
 	public MyPanel(){
-		this.setTitle("A07: Wenn wieder einmal Schnee fällt...");
+		this.setTitle("A09_Teamarbeit");
 		this.setLocationRelativeTo(null);
-		this.setSize(500, 300);
+		this.setSize(500, 500);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		model = new MyModel();
+		controller = new MyController();
 		
 		all = new JPanel();
 		north = new JPanel();
@@ -35,7 +41,17 @@ public class MyPanel extends JFrame {
 		this.add(north, BorderLayout.NORTH);//hier wird das north Panel .NORTH hinzugefügt
 		north.add(newGame);
 		
-		buttonArray = new JButton[25];
+		buttonArray = new JButton[5][5];
+		buttonsP.setLayout(new GridLayout(5 , 5));
+		this.add(buttonsP, BorderLayout.CENTER);
+		for(int x = 0 ; x < 4 ; x++){
+			for(int y = 0; y < 4;y++){
+				buttonArray[x][y]=new MyButton();
+				buttonArray[x][y].setActionCommand(x+","+y);
+				buttonArray[x][y].addActionListener(controller);
+			}
+		}
+		
 		this.setVisible(true);
 	}
 
