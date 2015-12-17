08c2adb79eeec40367404ba25971521258c8218a
rsauermann <rsauermann@student.tgm.ac.at>
2015-12-17 08:46:04 +0100

    erweiterung der Methode changeColor

--- src/smo/MyButton.java
+++ src/smo/MyButton.java
@@ -3,20 +3,30 @@ package smo;
 import java.awt.Color;
 
 import javax.swing.JButton;
+
 /**
  * Erweitert die Klasse JButton für die Farbänderung
+ * 
  * @author Raphael Sauermann
  *
  */
 public class MyButton extends JButton {
 	private boolean isOn;
-	public MyButton (){
+
+	public MyButton() {
 		super();
-		isOn=false;
+		isOn = false;
 		this.setBackground(Color.BLACK);
 	}
-	public changeColor(){
-		isOn=!isOn;
+
+	public void changeColor() {
+		isOn = !isOn;
+		if (isOn) {
+			this.setBackground(Color.YELLOW);
+		} else {
+			this.setBackground(Color.BLACK);
+		}
+
 	}
-	
+
 }
